b23bd53f1ad4e856ed1a95a68bedc366de673e64
Suvineet Singh <suvineet1998@gmail.com>
2024-02-19 21:13:16 +0000

    Add all the different cryptography implementation examples using ethereum-cryptography modules in JS

---
+++ cryptography/CSPRNG.js
@@ -0,0 +1,8 @@
+const { getRandomBytes} = require("ethereum-cryptography/random");
+const { bytesToHex} = require("ethereum-cryptography/utils");
+
+async function getCSPRN() {
+    console.log(bytesToHex(await getRandomBytes(32)));
+}
+
+getCSPRN();
